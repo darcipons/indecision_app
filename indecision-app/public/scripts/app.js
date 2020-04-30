45c9195ed1d828a2d5d5e6a019af1b6b60cb6df8
darcipons <darcimarie33@gmail.com>
2020-04-29 22:17:22 -0400

    added es6-classes-1 to playground

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,38 +1,37 @@
 'use strict';
 
-var visibility = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
-  title: 'Visibility Toggle',
-  caption: 'Hey, here are some details'
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var toggleVisibilty = function toggleVisibilty() {
-  visibility = !visibility;
-  render();
-};
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var render = function render() {
-  var visible = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    React.createElement(
-      'button',
-      { onClick: toggleVisibilty },
-      visibility ? 'Hide Details' : 'Show Details'
-    ),
-    visibility && React.createElement(
-      'p',
-      null,
-      app.caption
-    )
-  );
-  ReactDOM.render(visible, document.getElementById('app'));
-};
+    _classCallCheck(this, Person);
 
-render();
+    this.name = name;
+    this.age = age;
+  }
+
+  _createClass(Person, [{
+    key: 'getGretting',
+    value: function getGretting() {
+      return 'Hi, I am ' + this.name;
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' year(s) old.';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person('Darci Pons', 25);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
