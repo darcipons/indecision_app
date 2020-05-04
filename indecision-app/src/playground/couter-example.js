199d6d40f9dc423b5089d7c7899d492d0ddd7e75
darcipons <darcimarie33@gmail.com>
2020-05-04 15:45:00 -0400

    playground counter-example update buttons

--- indecision-app/src/playground/couter-example.js
+++ indecision-app/src/playground/couter-example.js
@@ -1,31 +1,65 @@
-let count = 0
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+  }
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
+  handleAddOne() {
+    console.log('adding')
+  }
+
+  handleMinusOne() {
+    console.log('subtracting')
+  }
+
+  handleReset() {
+    console.log('reset')
+  }
+  
+  render() {
+    return(
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    )
+  }
 }
 
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
+ReactDOM.render(<Counter/>, document.getElementById('app'))
+
+// let count = 0
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
+
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// }
+
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
 
 
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, appRoot);
-};
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   );
+//   ReactDOM.render(templateTwo, appRoot);
+// };
 
-renderCounterApp(); 
+// renderCounterApp(); 
