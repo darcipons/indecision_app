406e0e94e9464311f95091cc9d396ba5b7664b64
darcipons <darcimarie33@gmail.com>
2020-06-01 16:18:29 -0400

    added node modules and created option modal

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption'
 import Header from './Header'
 import Action from './Action'
 import Options from './Options'
+import OptionModal from './OptionModal'
 
 class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   }
 
   handleDeleteOptions = () => {
@@ -24,7 +26,7 @@ class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length)
     const option = this.state.options[randomNum]
-    alert(option)
+    this.setState(() => ({ selectedOption: option}))
   }
 
   handleAddOption = (option) => {
@@ -35,6 +37,10 @@ class IndecisionApp extends React.Component {
     }
 
     this.setState((prevState) => ({ options: prevState.options.concat(option)}))
+  }
+
+  handleClearSelectedOption = () => {
+    this.setState(() => ({selectedOption: undefined}))
   }
 
   componentDidMount() {
@@ -74,6 +80,10 @@ class IndecisionApp extends React.Component {
         />
         <AddOption 
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     );
---
+++ indecision-app/src/components/OptionModal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Modal from 'react-modal'
+
+const OptionModal = (props) => {
+  return (
+    <div>
+      <Modal
+        isOpen={!!props.selectedOption}
+        onRequestClose={props.handleClearSelectedOption}
+        contentLabel='Selected Option'
+      >
+        <h3>Selected Option</h3>
+        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <button onClick={props.handleClearSelectedOption}>Okay</button>
+      </Modal>
+    </div>
+  )
+}
+
+export default OptionModal
